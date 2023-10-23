3ff2dbc69fc0e8b5344c1195a42a5d41c947dfbd
brjatoba92 <brjatoba92@gmail.com>
2023-10-23 15:17:09 -0300

    Return Navegation and Local Storage

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
-const validateUser = async (email) => {
-    try{
-        const result = await fetch(
-            `https://mp-wallet-app-api.herokuapp.com/users?email=${email}`);
-        const user = await result.json();
-        return user;
-    } catch(error) {
-        return { error };
-    }
-};
-
-const onClickLogin = async () => {
-    const email = document.getElementById('input-email').value;
-    if (email.length < 5 || !email.includes('@')){
-        alert("Email invalido");
+const onClickLogin = () => {
+    const email = document.getElementById("input-email").value;
+    if (email.lenght <5 || !email.includes("@")){
+        alert("Email invalido")
         return;
     }
-    const result = await validateUser(email);
-    if (result.error){
-        alert('Falha ao validar email')
-        return;
-    }
-    localStorage.setItem("@WalletApp:userEmail", result.email);
-    localStorage.setItem("@WalletApp:userName", result.name);
-    localStorage.setItem("@WalletApp:userId", result.id);
-    window.open("./src/pages/home/index.html", "_self");
+    localStorage.setItem("@WalletApp:useEmail", email);
+    window.open("src/pages/home/index.html", "_self");
 };
