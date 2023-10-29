b1196e548c33165e4b11d27db7e1dca249f9ab8a
brjatoba92 <brjatoba92@gmail.com>
2023-10-28 23:15:45 -0300

    feat - implemented table with dates - first commit

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,56 @@
+const renderFinancesList = (data) => {
+    const table = document.getElementById("finances-table");
+    data.mapa((item) => {
+        const tableRow = document.createElement("tr");
+        tableRow.className = "mt smaller"
+        //title
+        const titleTd = document.createElement("td");
+        const titleText = document.createTextNode(item.title);
+        titleTd.appendChild(titleText);
+        tableRow.appendChild(titleTd);
+
+        //category
+        const categoryTd = document.createElement("td");
+        const categoryText = document.createTextNode(item.name);
+        categoryTd.appendChild(categoryText);
+        tableRow.appendChild(categoryTd);
+
+        //date
+        const dateTd = document.createElement("td")
+        const dateText = document.createTextNode(
+            new Date(item.date).toLocaleDateString()
+        );
+        dateTd.appendChild(dateText);
+        tableRow.appendChild(dateTd);
+
+        //value
+        const valueTd = document.createElement("td");
+        valueTd.className = "center"
+        const valueText = document.createTextNode(
+            new Intl.NumberFormat('pt-BR', { 
+            style: 'currency', 
+            currency: 'BRL', 
+            }).format(item.value)
+        );
+        valueTd.appendChild(valueText);
+        tableRow.appendChild(valueTd);
+
+        //delete
+
+        const deleteTd = document.createElement("td");
+        deleteTd.className = "right"
+        const deleteText = document.createTextNode("Deletar");
+        deleteTd.appendChild(deleteText);
+        tableRow.appendChild(deleteTd);
+
+        //table add tablerow
+
+        table.appendChild(tableRow)
+
+   });
+};
+
+
 const renderFinanceElements = (data) => {
     const totalItems = data.length;
     const revenues = data
@@ -64,6 +117,7 @@ const renderFinanceElements = (data) => {
     financeCard4.appendChild(balanceTextElement);
 };
 
+
 const onLoadFinancesData = async () => {
     try {
         const date = "2022-12-15";
@@ -79,6 +133,7 @@ const onLoadFinancesData = async () => {
         );
         const data = await result.json();
         renderFinanceElements(data)
+        renderFinancesList(data)
         return data;
     } catch (error) {
         return { error };
